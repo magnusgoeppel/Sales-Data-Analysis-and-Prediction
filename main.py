71df41643ba8d7ad50f92c599a74eb1808506bae
Magnus <magnus_goeppel@protonmail.com>
2024-05-31 19:08:45 +0200

    Set up project plan

---
+++ main.py
@@ -0,0 +1,36 @@
+# Imports
+import pandas as pd
+import numpy as np
+# ... other imports
+
+# 1. Initialisation and Data acquisition
+# 1.1. Create the Folder structure
+# 1.2. Load the data in a DataFrame
+
+# 2. Data exploration and cleaning
+# 2.1. Check the data (missing values, data types, etc.)
+# 2.2. Clean the data
+# 2.3. Explore the data (Histograms, Boxplots, Scatterplots, etc.)
+
+# 3. Feature engineering
+# 3.1. Create new features (e.g. from existing features)
+# 3.2. Transform the data (e.g. scaling, encoding, etc.)
+
+# 4. Data preprocessing
+# 4.1. Scale the data (if necessary)
+# 4.2. Split the data into training and test sets
+
+# 5. Model Building
+# 5.1. Choose a model (e.g. Linear Regression, Random Forest, etc.)
+# 5.2. Configure the model (e.g. hyperparameters)
+# 5.3. Train the model
+
+# 6. Performance evaluation and Visualization
+# 6.1. Evaluate the model (e.g. RMSE, R^2, etc.)
+# 6.2. Visualize the model (e.g. feature importance, predictions, etc.)
+
+# 7. Dokumentation and Presentation
+# 7.1. Comment the code
+# 7.2. Create the Ananconda environment file (environment.yml)
+# 7.3. Write the README file
+# 7.4. Create the presentation
