de1ace0618fcbfdbc802dbef13c501a25a8d5220
Magnus <magnus_goeppel@protonmail.com>
2024-11-24 14:19:38 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,75 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+
+from scripts.data_cleaning_exploration import check_and_clean_data, explore_data
+from scripts.data_preprocessing import create_features, save_transformed_data, encode, scale_x, scale_y
+from scripts.model_building import train_model, performance_evaluation, hyperparameter_tuning, plot_decision_tree, \
+    plot_actual_vs_predicted_values, format_model_params
+
+# Display all columns
+pd.set_option('display.max_columns', None)
+
+
+# 1. Data acquisition (load the data)
+data = pd.read_csv("data/sales_data_sample.csv", encoding="latin1")  # encoding non ascii characters
+df = pd.DataFrame(data)
+
+# 2. Data exploration and cleaning
+
+# 2.1. Check and clean the data
+df = check_and_clean_data(df)
+
+# 2.2. Save the cleaned data to a new CSV file
+df.to_csv("data/cleaned_sales_data.csv", index=False)
+
+# 2.3. Explore the data
+explore_data(df)
+
+
+# 3. Data preprocessing
+
+# 3.1. Feature and target selection
+x = create_features(df)
+y = df["QUANTITYORDERED"]
+
+# 3.2. Save the transformed data to a new CSV file
+save_transformed_data(x, y, "data/transformed_sales_data.csv")
+
+# 3.3. Encode non-numeric data
+x = encode(x)
+
+# 3.4. Scale the data
+x = scale_x(x)
+y = scale_y(y)
+
+# 3.5. Split the data into training and test sets (80% training, 20% test)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+# 3.6. Split the training data into training and validation sets (65% training, 15% validation)
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.1875, random_state=42)
+
+
+# 4. Model Building
+
+# 4.1. Select Regression Tree as the model
+model = DecisionTreeRegressor(random_state=42)
+
+# 4.2. Hyperparameter tuning
+model = hyperparameter_tuning(model, x_val, y_val)
+
+# 4.3. Model parameters
+formatted_params = format_model_params(model)
+print(f"\033[1mModel Parameters:\033[0m\n{formatted_params}\n")
+
+# 4.4. Build and train the model with 5-fold cross validation
+model, y_tests, y_preds = train_model(model, x_train, y_train, 5)
+
+# 4.5. Performance evaluation
+metrics = performance_evaluation(y_tests, y_preds)
+print(f"\033[1mPerformance Evaluation:\033[0m\n{metrics}")
+
+# 4.5. Visualize the model
+plot_decision_tree(model, x.columns)
+plot_actual_vs_predicted_values(y_tests, y_preds)
---
+++ scripts/data_cleaning_exploration.py
@@ -0,0 +1,130 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def check_and_clean_data(df):
+    # Missing values
+    missing_values = df.isnull().sum()
+
+    if missing_values.any():
+        df = df.replace("", np.nan)
+
+    # Splite ORDERDATE into date and time
+    df["ORDERDATE"] = pd.to_datetime(df["ORDERDATE"]).dt.date
+    df["ORDERTIME"] = pd.to_datetime(df["ORDERDATE"]).dt.time
+
+    # Check if ORDERTIME column contains only times set to '00:00:00'
+    if (df['ORDERTIME'] == pd.to_datetime('00:00:00').time()).all():
+        df = df.drop(columns=['ORDERTIME'])
+
+    # Check for duplicates
+    duplicates = df.duplicated().sum()
+
+    if duplicates:
+        df = df.drop_duplicates()
+
+    # Check for non ascii characters
+    non_ascii_chars_dict = {}
+
+    # Iterating over all columns and rows to find non-ascii characters
+    for col in df.columns:
+        for row in df.index:
+            value = str(df.at[row, col])
+            for char in value:
+                if ord(char) > 127:
+                    if char not in non_ascii_chars_dict:
+                        non_ascii_chars_dict[char] = (value, col, row)
+
+    """
+    # Print non-ascii characters
+    print("Non-Ascii characters:")
+    for char, (value, col, row) in non_ascii_chars_dict.items():
+        print(f"Row: {row+1}, {col}: {value}, Character: {char}")
+    """
+
+    # Replace Non-Ascii characters
+    replacement_list = ['ae', 'y', 'i']
+    replacement_dict = {}
+
+    # Assign replacements from the list to the non-ASCII characters
+    for i, char in enumerate(non_ascii_chars_dict.keys()):
+        if i < len(replacement_list):
+            replacement_dict[char] = replacement_list[i]
+
+    # Replace all non-ascii characters in the DataFrame
+    for col in df.columns:
+        for row in df.index:
+            value = str(df.at[row, col])
+            new_value_chars = []
+            for char in value:
+                new_char = replacement_dict.get(char, char)
+                new_value_chars.append(new_char)
+            new_value = ''.join(new_value_chars)
+            df.at[row, col] = new_value
+
+    # Assign data types to the columns
+    numeric_cols = ['QUANTITYORDERED', 'PRICEEACH', 'SALES', 'QTR_ID', 'MONTH_ID', 'YEAR_ID', 'MSRP']
+    categorical_cols = ['ORDERNUMBER', 'ORDERLINENUMBER', 'ORDERDATE', 'STATUS', 'PRODUCTLINE', 'PRODUCTCODE',
+                        'CUSTOMERNAME', 'PHONE', 'ADDRESSLINE1', 'ADDRESSLINE2', 'CITY', 'STATE', 'POSTALCODE',
+                        'COUNTRY', 'TERRITORY', 'CONTACTLASTNAME', 'CONTACTFIRSTNAME', 'DEALSIZE']
+
+    # Iterate over columns and assign data types
+    for col in df.columns:
+        if col in numeric_cols:
+            df[col] = pd.to_numeric(df[col], errors='coerce').astype('float64')
+        elif col in categorical_cols:
+            df[col] = df[col].astype('category')
+
+    return df
+
+
+# Explore the data
+def explore_data(df):
+    # Numeric Summaries
+    numeric_summary = df.describe()
+    print("\033[1mNumeric Summaries:\033[0m")
+    print(round(numeric_summary, 3))
+    print()
+
+    # Set the aesthetic style of the plots
+    sns.set_style("whitegrid")
+
+    # Boxplots
+    numeric_cols = df.select_dtypes(include=['float64']).columns
+    plt.figure(figsize=(15, 10))
+    for i, col in enumerate(numeric_cols):
+        plt.subplot(3, 3, i + 1)
+        sns.boxplot(y=df[col])
+        plt.title(f'Boxplot of {col}')
+    plt.tight_layout(rect=[0, 0, 1, 0.96])
+    plt.suptitle('Boxplots of Numeric Columns', fontsize=20)
+    plt.show()
+
+    # Scatterplots
+    plt.figure(figsize=(15, 10))
+    for i, col in enumerate(numeric_cols):
+        if col != 'QUANTITYORDERED':
+            plt.subplot(3, 3, i + 1)
+            sns.scatterplot(x=df[col], y=df['QUANTITYORDERED'])
+            plt.title(f'Scatterplot of {col} vs QUANTITYORDERED')
+    plt.tight_layout(rect=[0, 0, 1, 0.96])
+    plt.suptitle('Scatterplots of Numeric Columns vs QUANTITYORDERED', fontsize=20)
+    plt.show()
+
+    # Density Plots
+    # Exclude columns that are not suitable for density plots
+    exclude_cols = {'QTR_ID', 'MONTH_ID', 'YEAR_ID'}
+
+    plt.figure(figsize=(15, 10))
+
+    # Plotting density plots for numeric columns excluding specified columns
+    for i, col in enumerate(col for col in numeric_cols if col not in exclude_cols):
+        plt.subplot(3, 3, i + 1)
+        sns.kdeplot(df[col], fill=True)
+        plt.title(f'Density Plot of {col}')
+
+    plt.tight_layout(rect=[0, 0, 1, 0.96])
+    plt.suptitle('Density Plots of Numeric Columns', fontsize=20)
+    plt.show()
